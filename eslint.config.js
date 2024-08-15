6ca58948b8333dc2ddc8f1a80584615b9ae7eaea
Berador Stambekov <stambekovbera@gmail.com>
2024-08-15 12:36:31 +0500

    fix: поправка eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import unusedImports from "eslint-plugin-unused-imports";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import stylisticTs from '@stylistic/eslint-plugin-ts'
+import js from '@eslint/js';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
+  files: ['**/*.{js,ts,tsx}'],
+  ignores: ['dist', 'node_modules', '.idea', '.vscode'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
@@ -18,8 +18,8 @@ export default tseslint.config({
   plugins: {
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
-    "unused-imports": unusedImports,
-    "simple-import-sort": simpleImportSort,
+    'unused-imports': unusedImports,
+    'simple-import-sort': simpleImportSort,
     '@stylistic/ts': stylisticTs
   },
   rules: {
@@ -28,22 +28,22 @@ export default tseslint.config({
       'warn',
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-unused-vars": "off",
-    "no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
       {
-        "vars": "all",
-        "varsIgnorePattern": "^_",
-        "args": "after-used",
-        "argsIgnorePattern": "^_",
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_',
       },
     ],
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "@stylistic/ts/indent": ['error', 2],
-    "@stylistic/ts/semi": ['error'],
-    "@stylistic/ts/quotes": ['error', "single"],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@stylistic/ts/indent': ['error', 2],
+    '@stylistic/ts/semi': ['error'],
+    '@stylistic/ts/quotes': ['error', 'single'],
   },
-})
+});
