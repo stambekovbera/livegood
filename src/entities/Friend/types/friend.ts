795d5edff27dbc6d6f982234931708158688c2bf
Berador Stambekov <stambekovbera@gmail.com>
2024-08-16 11:29:33 +0500

    feat: добавил обычный список друзей

--- src/entities/Friend/types/friend.ts
+++ src/entities/Friend/types/friend.ts
@@ -1,6 +1,11 @@
-export interface IFriendNode {
+export interface IFriendNode extends IFriend{
   left: IFriendNode | null;
   right: IFriendNode | null;
+}
+
+export interface IFriend {
   avatar: string;
   username: string;
+  friendsCount: number;
+  lgCoinsAmount: number;
 }
--- src/widgets/FriendsPageBottom/FriendsPageBottom.tsx
+++ src/widgets/FriendsPageBottom/FriendsPageBottom.tsx
@@ -3,6 +3,7 @@ import cn from 'classnames';
 import React, { useState } from 'react';
 
 import { FriendsMatrix, FriendsVariantTabs } from '@/entities/Friend';
+import { FriendsList } from '@/entities/Friend/ui/FriendsList/FriendsList';
 import { CopyInviteButton } from '@/features/CopyInviteButton/ui/CopyInviteButton';
 
 import classes from './FriendsPageBottom.module.scss';
@@ -23,10 +24,13 @@ export const FriendsPageBottom: React.FC<IFriendsPageBottomProps> = (props) => {
   };
 
   return (
-    <Box className={ cn( classes.wrapper, {}, [ className ] ) }>
+    <Box className={ cn( classes.wrapper, {
+      [`${ classes.matrixVariant }`]: activeVariant === 'matrix',
+    }, [ className ] ) }>
       <Box className={ classes.top }>
         <FriendsVariantTabs value={ activeVariant } onChange={ handleChangeActiveVariant }/>
         {activeVariant === 'matrix' && <FriendsMatrix/>}
+        {activeVariant === 'list' && <FriendsList/>}
       </Box>
       <Box className={classes.bottom}>
         <CopyInviteButton/>
